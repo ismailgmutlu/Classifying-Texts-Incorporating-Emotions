6e3ac06b4465d6ae2c03116335c27a3d0581b41d
ismailgmutlu <ismailgmutlu@gmail.com>
2018-12-24 13:17:13 +0300

    reset

--- HuzunWebsite/dbinfo.php
+++
@@ -1,6 +0,0 @@
-<?php
-$GLOBALS['servername'] = "huzuninstance.chezocpbjx4z.eu-central-1.rds.amazonaws.com";
-$GLOBALS['username'] = "masterhuzun";
-$GLOBALS['password'] = "123456789Ai!";
-$GLOBALS['dbname'] = "masterhuzun";
-?>
