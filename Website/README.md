39d2fa40555c2fdc297d3b53c5e356a3970cb04a
ismailgmutlu <ismailgmutlu@gmail.com>
2019-01-03 16:50:53 +0300

    added website

---
+++ Website/README.md
@@ -0,0 +1,3 @@
+Project Website 
+www.huzun.biz - Main Page for crowdsourcing data
+www.huzun.biz/test.php - For demonstration
---
+++ Website/dbinfo.php
@@ -0,0 +1,6 @@
+<?php
+$GLOBALS['servername'] = "huzuninstance.chezocpbjx4z.eu-central-1.rds.amazonaws.com";
+$GLOBALS['username'] = "masterhuzun";
+$GLOBALS['password'] = "123456789Ai!";
+$GLOBALS['dbname'] = "masterhuzun";
+?>
